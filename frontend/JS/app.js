261a9b0a0e6693008a3f94ee53f352422777f5fe
PalashSinha14 <palashsinha14@gmail.com>
2026-03-08 12:19:34 +0530

    Added frontend pages and Render compatible port config

---
+++ frontend/JS/app.js
@@ -0,0 +1,61 @@
+// LOGIN
+const loginForm = document.getElementById("loginForm");
+
+if(loginForm){
+loginForm.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const res = await fetch("/login",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+email:email,
+password:password
+})
+});
+
+const data = await res.json();
+
+alert(JSON.stringify(data));
+
+});
+}
+
+
+// SIGNUP
+const signupForm = document.getElementById("signupForm");
+
+if(signupForm){
+
+signupForm.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const res = await fetch("/signup",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+name:name,
+email:email,
+password:password
+})
+});
+
+const data = await res.json();
+
+alert(JSON.stringify(data));
+
+});
+}
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/palashsinha14/go-rest-api/db"
+	"github.com/palashsinha14/go-rest-api/routes"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.Static("/static", "./frontend")
+	server.LoadHTMLGlob("frontend/*.html")
+
+	// Frontend routes
+	server.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+	server.GET("/login-page", func(c *gin.Context) {
+		c.HTML(200, "login.html", nil)
+	})
+	server.GET("/signup-page", func(c *gin.Context) {
+		c.HTML(200, "signup.html", nil)
+	})
+
+	routes.RegisterRoutes(server)
+
+	// Render requires dynamic port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	// Start server
+	server.Run(":" + port)
+}
+
+/*
+package main
+
 //import "fmt"
 
 import (
@@ -15,3 +55,4 @@ func main() {
 	routes.RegisterRoutes(server)
 	server.Run(":8080")
 }
+*/
